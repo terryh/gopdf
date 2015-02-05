216fe3da1d9266c6167f94f3ed770b774b32532d
Terry Huang <terryh.tp@gmail.com>
2015-02-05 15:25:17 +0800

    Url2jpeg

--- README.md
+++ README.md
@@ -7,28 +7,34 @@ golang wrap for wkhtmltopdf command.
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/terryh/gopdf"
+	"github.com/terryh/gopdf"
 )
 
 func Pdfhandle(w http.ResponseWriter, r *http.Request) {
-    result, err := gopdf.Url2pdf("http://nvd3.org/examples/stackedArea.html")
-    fmt.Println(err)
-    w.Header().Set("Content-Type", "application/pdf")
-    w.Write(result)
+	result, err := gopdf.Url2pdf("http://nvd3.org/examples/stackedArea.html")
+	fmt.Println(err)
+	w.Header().Set("Content-Type", "application/pdf")
+	w.Write(result)
+}
+
+func Jpeghandle(w http.ResponseWriter, r *http.Request) {
+	result, err := gopdf.Url2jpeg("http://nvd3.org/examples/stackedArea.html")
+	fmt.Println(err)
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Write(result)
 }
 
 func main() {
 
-    http.HandleFunc("/", Pdfhandle)
+	http.HandleFunc("/", Pdfhandle)
+	http.HandleFunc("/jpeg", Jpeghandle)
 
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 
 }
-
-
 ```
 
 
--- wkhtmltopdf.go
+++ wkhtmltopdf.go
@@ -9,17 +9,26 @@ import (
 )
 
 var (
-	wkhtmltopdfCmd = "wkhtmltopdf" // the command to call wkhtmltopdf command
+	wkhtmltopdfCmd   = "wkhtmltopdf"   // the command to call wkhtmltopdf command
+	wkhtmltoimageCmd = "wkhtmltoimage" // the command to call wkhtmltoimage command
 
-	haveWK    = "Looks like you have wkhtmltopdf install at your system path."
-	nothaveWK = `Looks you don't have wkhtmltopdf install at your system,
+	haveWKPdf   = "Looks like you have wkhtmltopdf install at your system path."
+	haveWKImage = "Looks like you have wkhtmltoimage install at your system path."
+	nothaveWK   = `Looks you don't have wkhtmltopdf install at your system,
 please take a look on http://wkhtmltopdf.org/downloads.html`
-	wkcheck = false
+
+	wkpdfcheck   = false
+	wkimagecheck = false
 )
 
 func SetWkhtmltopdf(cmd string) error {
 	wkhtmltopdfCmd = cmd
 	return CheckWkhtmltopdf(cmd)
+}
+
+func SetWkhtmltoimage(cmd string) error {
+	wkhtmltoimageCmd = cmd
+	return CheckWkhtmltoimage(cmd)
 }
 
 func CheckWkhtmltopdf(cmd string) error {
@@ -33,20 +42,38 @@ func CheckWkhtmltopdf(cmd string) error {
 		return nil
 	}
 
-	fmt.Println(haveWK)
+	fmt.Println(haveWKPdf)
 	// seems haaving wkhtmltopdf
-	wkcheck = true
+	wkpdfcheck = true
+	return nil
+}
+
+func CheckWkhtmltoimage(cmd string) error {
+
+	fmt.Println("Checking wkhtmltoimage..")
+
+	_, err := exec.Command(cmd, "-V").Output()
+
+	if err != nil {
+		fmt.Println(nothaveWK)
+		return nil
+	}
+
+	fmt.Println(haveWKImage)
+	// seems haaving wkhtmltopdf
+	wkimagecheck = true
 	return nil
 }
 
 func init() {
 	CheckWkhtmltopdf(wkhtmltopdfCmd)
+	CheckWkhtmltoimage(wkhtmltoimageCmd)
 }
 
 // Url2pdf is mapping wkhtmltopdf url with args
 // and have result from stdout wrap in []byte
 func Url2pdf(url string, args ...string) ([]byte, error) {
-	if !wkcheck {
+	if !wkpdfcheck {
 		return nil, errors.New(nothaveWK)
 	}
 
@@ -64,6 +91,37 @@ func Url2pdf(url string, args ...string) ([]byte, error) {
 	var out bytes.Buffer
 
 	cmd := exec.Command(wkhtmltopdfCmd, cmdSlice...)
+	cmd.Stdout = &out
+	err := cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return out.Bytes(), err
+
+}
+
+// Url2image is mapping wkhtmltoimage url with args
+// and have result from stdout wrap in []byte
+// the default format is JPEG
+func Url2jpeg(url string, args ...string) ([]byte, error) {
+	if !wkimagecheck {
+		return nil, errors.New(nothaveWK)
+	}
+
+	cmdSlice := []string{url}
+
+	for _, line := range args {
+		cmdSlice = append(cmdSlice, line)
+	}
+
+	// pipe to stdout
+	cmdSlice = append(cmdSlice, "-")
+
+	var out bytes.Buffer
+
+	cmd := exec.Command(wkhtmltoimageCmd, cmdSlice...)
 	cmd.Stdout = &out
 	err := cmd.Run()
 
