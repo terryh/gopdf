cf9af93cbc9a86329d8907fdf2c3e73dfc00ff87
Terry Huang <terryh.tp@gmail.com>
2015-02-25 17:26:54 +0800

    fix option order

--- wkhtmltopdf.go
+++ wkhtmltopdf.go
@@ -110,11 +110,13 @@ func Url2jpeg(url string, args ...string) ([]byte, error) {
 		return nil, errors.New(nothaveWK)
 	}
 
-	cmdSlice := []string{url}
+	cmdSlice := []string{}
 
 	for _, line := range args {
 		cmdSlice = append(cmdSlice, line)
 	}
+
+	cmdSlice = append(cmdSlice, url)
 
 	// pipe to stdout
 	cmdSlice = append(cmdSlice, "-")
